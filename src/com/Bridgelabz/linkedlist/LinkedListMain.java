6ed9ad30314f6e5cc896718919eb886060119915
Sujay509 <sujaychauhan98@gmail.com>
2022-10-31 08:42:34 +0530

    Welcome to Kinked list

---
+++ src/com/Bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,15 @@
+package com.Bridgelabz.linkedlist;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Welcome to linked list problem");
+
+	        LinkedList linkedList = new LinkedList();
+	        linkedList.createLinkedList();
+	        linkedList.displayLinkedList();
+
+	}
+
+}
